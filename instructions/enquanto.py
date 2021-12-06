1e3c3e48cc266817d449dd6d835663857c3c8bb4
Didas72 <diogocruzdiniz@gmail.com>
2021-12-06 13:34:47 +0000

    Added enquanto and more math functions

---
+++ instructions/enquanto.py
@@ -0,0 +1,4 @@
+from instructions.se import se
+
+def enquanto(parts, variaveis):
+    return se(parts, variaveis)
--- main.py
+++ main.py
@@ -10,14 +10,16 @@ def runCode(lines):
     variaveis = {}
     linha = 0
     nestLevel = 0
+    lastCondition = False
     loopStack = []
 
     from instructions.escrever import escrever
     from instructions.ler import ler
     from instructions.guardar import guardar
     from instructions.se import se
+    from instructions.enquanto import enquanto
 
-    while (True):
+    while (linha < len(lines)):
         parts = []
         remain = lines[linha].strip()
 
@@ -44,41 +46,70 @@ def runCode(lines):
         if (len(lines[linha]) > 1):
             instruction = parts[0].casefold()
 
-            if (nestLevel == 0):
-                if (instruction == "escrever"):
-                    escrever(parts, variaveis)
-                elif (instruction == "ler"):
-                    ler(parts, variaveis)
-                elif (instruction == "guardar"):
-                    guardar(parts, variaveis)
-                elif (instruction == "se"):
-                    if (se(parts, variaveis)):
-                        nestLevel = 0
+            try:
+                if (nestLevel == 0):
+                    if (instruction == "escrever"):
+                        escrever(parts, variaveis)
+                    elif (instruction == "ler"):
+                        ler(parts, variaveis)
+                    elif (instruction == "guardar"):
+                        guardar(parts, variaveis)
+                    elif (instruction == "se"):
+                        if (se(parts, variaveis)):
+                            nestLevel = 0
+                            lastCondition = True
+                        else:
+                            nestLevel = 1
+                            lastCondition = False
+                    elif (instruction == "enquanto"):
+                        if (enquanto(parts, variaveis)):
+                            loopStack.append(linha)
+                        else:
+                            nestLevel = -1
+                    elif (instruction == "fim"):
+                        if (parts[1] == "da" and parts[2] == "condicao"):
+                            nestLevel = 0
+                        elif (parts[1] == "de" and parts[2] == "enquanto"):
+                            try:
+                                linha = loopStack.pop() - 1
+                            except:
+                                print("Não podes por fim de enquanto sem comecar um enquanto!")
+                                exit()
+                    elif (instruction == "senao"):
+                        if (lastCondition):
+                            nestLevel = 1
+                        else:
+                            nestLevel = 0
+                        pass
+                    elif (instruction.startswith("!")):
+                        pass
+                    elif (instruction == "sair"):
+                        break
                     else:
-                        nestLevel = 1
-                elif (instruction == "fim"):
-                    if (parts[1] == "da" and parts[2] == "condicao"):
-                        nestLevel = 0
-                    elif (parts[1] == "de" and parts[2] == "enquanto"):
-                        linha = loopStack.pop() - 1
-                elif (instruction.startswith("!")):
-                    continue
+                        print(parts[0], "não é uma instrução válida! Se quiseres por um comentário, começa a linha com '!'")
+                        exit()
                 else:
-                    print(parts[0], "não é uma instrução válida! Se quiseres por um comentário, começa a linha com '!'")
-                    exit()
-            else:
-                if (instruction == "fim"):
-                    if (parts[1] == "da" and parts[2] == "condicao"):
-                        nestLevel -= 1
-                    elif (parts[1] == "de" and parts[2] == "enquanto"):
-                        #Ignore
-                        continue
-                elif (instruction == "se"):
-                    nestLevel += 1
+                    if (nestLevel > 0): #nestLevel de um se
+                        if (instruction == "fim"):
+                            if (parts[1] == "da" and parts[2] == "condicao"):
+                                nestLevel -= 1
+                        elif (instruction == "se"):
+                            nestLevel += 1
+                        elif (instruction == "senao"):
+                            nestLevel -= 1
+                    else: #nestLevel de um enquanto
+                        if (instruction == "fim"):
+                            if (parts[1] == "de" and parts[2] == "enquanto"):
+                                nestLevel += 1
+                        elif (instruction == "enquanto"):
+                            nestLevel -= 1
+            except:
+                print(lines[linha], "deu um erro!")
+                exit()
 
         linha += 1
     
-    print("End")
+    print("Fim do programa")
     exit()
 
 lines = loadLines("joel.txt")
